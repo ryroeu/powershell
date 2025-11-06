8575355b1d910d3fc2a4a5e3c0ab4ff99b66bf4a
ryan <38826351+ryroeu@users.noreply.github.com>
2025-11-06 02:29:24 +0100

    powershell

--- ChocolateyBasic.ps1
+++ ChocolateyBasic.ps1
@@ -4,107 +4,107 @@ Set-ExecutionPolicy Unrestricted -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Chocolatey Extensions and Updates
-choco install chocolatey-core.extension
-choco install chocolateygui
-choco install chocolateypackageupdater
+choco install chocolatey-core.extension -y
+choco install chocolateygui -y
+choco install chocolateypackageupdater -y
 
 # AWS
-choco install awscli
-choco install awstools.powershell
+choco install awscli -y
+choco install awstools.powershell -y
 
 # Azure
-choco install az.powershell
-choco install azure-cli
+choco install az.powershell -y
+choco install azure-cli -y
 
 # Browsers
-choco install brave
-choco install firefox
-choco install googlechrome
-choco install waterfox
+choco install brave -y
+choco install firefox -y
+choco install googlechrome -y
+choco install waterfox -y
 
 # DevOps
-choco install python
-choco install sudo
+choco install python -y
+choco install sudo -y
 
 # Docker
-choco install docker-cli
+choco install docker-cli -y
 
 # Gaming
-choco install discord
+choco install discord -y
 
 # Git
-choco install gh
-choco install github-desktop
-choco install git
-choco install git-credential-manager-for-windows
+choco install gh -y
+choco install github-desktop -y
+choco install git -y
+choco install git-credential-manager-for-windows -y
 
 # HashiCorp
-choco install consul
-choco install packer
-choco install terraform
-choco install vagrant
-choco install vault
+choco install consul -y
+choco install packer -y
+choco install terraform -y
+choco install vagrant -y
+choco install vault -y
 
 # Java
-choco install openjdk
+choco install openjdk -y
 
 # Microsoft
-choco install dotnet
-choco install dotnetfx
-choco install microsoft-teams
-choco install nugetpackagemanager
-choco install office365business
-choco install powershell-core
-choco install powertoys
-choco install psexec
-choco install pstools
-choco install rsat
-choco install sql-server-management-studio
-choco install sysinternals
-choco install vcredist140
-choco install vscode
-choco install vscode-ansible
-choco install vscode-java
-choco install vscode-powershell
-choco install vscode-yaml
+choco install dotnet -y
+choco install dotnetfx -y
+choco install microsoft-teams -y
+choco install nugetpackagemanager -y
+choco install office365business -y
+choco install powershell-core -y
+choco install powertoys -y
+choco install psexec -y
+choco install pstools -y
+choco install rsat -y
+choco install sql-server-management-studio -y
+choco install sysinternals -y
+choco install vcredist140 -y
+choco install vscode -y
+choco install vscode-ansible -y
+choco install vscode-java -y
+choco install vscode-powershell -y
+choco install vscode-yaml -y
 
 #PDQ
-choco install pdq-deploy
-choco install pdq-inventory
+choco install pdq-deploy -y
+choco install pdq-inventory -y
 
 # Proton
-choco install protonvpn
+choco install protonvpn -y
 
 # vmWare
-choco install vmrc
-choco install vmware-powercli-psmodule
-choco install vmwarevsphereclient
-choco install vmwareworkstation
+choco install vmrc -y
+choco install vmware-powercli-psmodule -y
+choco install vmwarevsphereclient -y
+choco install vmwareworkstation -y
 
 # Zoom
-choco install zoom
+choco install zoom -y
 
 # Utilities - FTP
-choco install filezilla
-choco install winscp
+choco install filezilla -y
+choco install winscp -y
 
 # Utilities - Network
-choco install advanced-ip-scanner
-choco install nmap
-choco install wireshark
+choco install advanced-ip-scanner -y
+choco install nmap -y
+choco install wireshark -y
 
 # Utilities - Remote
-choco install openvpn
-choco install putty
-choco install royalts-v7-arm64
+choco install openvpn -y
+choco install putty -y
+choco install royalts-v7-arm64 -y
 
 # Miscellaneous
-choco install 7zip
-choco install adobereader
-choco install authy-desktop
-choco install crystaldiskinfo
-choco install dependencywalker
-choco install greenshot
-choco install speedtest
-choco install treesizefree
-choco install utorrent
+choco install 7zip -y
+choco install adobereader -y
+choco install authy-desktop -y
+choco install crystaldiskinfo -y
+choco install dependencywalker -y
+choco install greenshot -y
+choco install speedtest -y
+choco install treesizefree -y
+choco install utorrent -y
