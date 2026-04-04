0d0a1d7e0631b4bfcb942fc711b28a3fa1361f09
ryan <38826351+ryroeu@users.noreply.github.com>
2026-04-04 16:00:33 +0200

    code update for new version

--- PSInstall.ps1
+++ PSInstall.ps1
@@ -3,6 +3,6 @@
     Installs PowerShell.
 #>
 
-Invoke-WebRequest -Uri https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/PowerShell-7.4.2-win-x64.msi -Outfile C:\Temp\PowerShell-7.4.2.msi
+Invoke-WebRequest -Uri https://github.com/PowerShell/PowerShell/releases/download/v7.6.0/PowerShell-7.6.0-win-x64.msi -Outfile C:\Temp\PowerShell-7.6.0.msi
 Set-Location C:\Temp 
-.\PowerShell-7.4.2.msi
+.\PowerShell-7.6.0.msi
